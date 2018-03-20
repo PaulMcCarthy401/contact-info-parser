147063649be8465d5e0816b22e77b54e389fa438
PaulMcCarthy401 <paul.mccarthy401@gmail.com>
2018-03-20 14:03:11 -0400

    Add contact info class and parser stub.

---
+++ BusinessCardParser.py
@@ -0,0 +1,8 @@
+class ContactInfo:
+	def __init__(self, name, phoneNumber, emailAddress):
+		self.name = name
+		self.phoneNumber = phoneNumber
+		self.emailAddress = emailAddress
+
+def getContactInfo(document):
+	pass
