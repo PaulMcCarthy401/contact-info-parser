f856c1b8c7e29695e447f8c4c86f67f02c3e06f1
PaulMcCarthy401 <paul.mccarthy401@gmail.com>
2018-03-21 04:01:52 -0400

    BusinessCardParser module to ocrparser and handle model load errors.

--- ocrparser.py
+++ ocrparser.py
@@ -6,10 +6,16 @@ class ContactInfo:
 		self.name = name
 		self.phoneNumber = phoneNumber
 		self.emailAddress = emailAddress
+	
+	def __str__(self):
+		return "{}, {}, {}".format(self.name, self.phoneNumber, self.emailAddress)
 
 class BusinessCardParser:
 	def __init__(self, model='en_core_web_sm'):
-		self.nlp = spacy.load(model)
+		try:
+			self.nlp = spacy.load(model)
+		except:
+			raise ImportError("Spacy model could not be loaded.")
 
 	def getContactInfo(self, document):
 		"""Parse a document and return a ContactInfo object containing parsed values
@@ -25,4 +31,4 @@ class BusinessCardParser:
 			
 			for ent in doc.ents:
 				if ent.label_ == 'PERSON':
-					return ent.text
+					return ContactInfo(ent.text, '', '')
