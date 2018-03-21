e4d28d3e84ba667edd393bf0103f38423eb17f34
PaulMcCarthy401 <paul.mccarthy401@gmail.com>
2018-03-21 01:29:47 -0400

    Implement name detection in getContactInfo.

--- BusinessCardParser.py
+++ BusinessCardParser.py
@@ -1,3 +1,9 @@
+import os
+import spacy
+
+
+nlp = spacy.load('en_core_web_sm')
+
 class ContactInfo:
 	def __init__(self, name, phoneNumber, emailAddress):
 		self.name = name
@@ -5,4 +11,21 @@ class ContactInfo:
 		self.emailAddress = emailAddress
 
 def getContactInfo(document):
-	pass
+	"""Parse a document and return a ContactInfo object containing parsed values
+	
+	Args:
+		document (str): The raw contact info string
+	
+	Returns:
+		ContactInfo
+	"""
+	name = ''
+	phoneNumber = ''
+	emailAddress = ''
+
+	for line in document.split('\n'):
+		doc = nlp(line)
+		
+		for ent in doc.ents:
+			if ent.label_ == 'PERSON':
+				return ent.text
