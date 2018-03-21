f906feb1c29f758eb7d831239a26e1d3a91cb6ed
PaulMcCarthy401 <paul.mccarthy401@gmail.com>
2018-03-21 02:30:30 -0400

    Add configurable model selection to card parser.

--- BusinessCardParser.py
+++ BusinessCardParser.py
@@ -1,8 +1,5 @@
-import os
 import spacy
 
-
-nlp = spacy.load('en_core_web_sm')
 
 class ContactInfo:
 	def __init__(self, name, phoneNumber, emailAddress):
@@ -10,22 +7,22 @@ class ContactInfo:
 		self.phoneNumber = phoneNumber
 		self.emailAddress = emailAddress
 
-def getContactInfo(document):
-	"""Parse a document and return a ContactInfo object containing parsed values
-	
-	Args:
-		document (str): The raw contact info string
-	
-	Returns:
-		ContactInfo
-	"""
-	name = ''
-	phoneNumber = ''
-	emailAddress = ''
+class BusinessCardParser:
+	def __init__(self, model='en_core_web_sm'):
+		self.nlp = spacy.load(model)
 
-	for line in document.split('\n'):
-		doc = nlp(line)
+	def getContactInfo(self, document):
+		"""Parse a document and return a ContactInfo object containing parsed values
 		
-		for ent in doc.ents:
-			if ent.label_ == 'PERSON':
-				return ent.text
+		Args:
+			document (str): The raw contact info string
+		
+		Returns:
+			ContactInfo
+		"""
+		for line in document.split('\n'):
+			doc = self.nlp(line)
+			
+			for ent in doc.ents:
+				if ent.label_ == 'PERSON':
+					return ent.text
