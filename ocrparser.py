65b555f709bf253a6271586a4f2c61345002819d
PaulMcCarthy401 <paul.mccarthy401@gmail.com>
2018-03-23 14:04:30 -0400

    Add regex documentation.

--- ocrparser.py
+++ ocrparser.py
@@ -42,11 +42,31 @@ class BusinessCardParser:
 
 		return None
 
-	def getPhoneNumber(self, text):
+	def getPhoneNumber(self, text, excludeFax=True):
 		"""Parse a single card and return a string containing the phone number,
 			or None, if no phone number was found. The phone number will be
 			returned without formatting."""
-		match = re.search(r'(?:\d{1,3}.*)?\d{3}.*\d{3}.*\d{4}', text)
+
+		"""Regex: (where brackets indicated matches)
+
+		^(?!Fax).*(?:\d{1,3}.*)?\d{3}.*\d{3}.*\d{4}
+
+
+		^(?!Fax).* - OPTIONAL - Exclude strings which start with "Fax"
+
+		(?:\d{1,3}.*)? - Include area code in match, if it exists
+			Ex. [1]-123-456-7890
+
+		\d{3}.* - Match block of 3 numbers:
+			Ex. [123]-[456]-7890
+
+		\d{3}.* - Match block of 3 numbers:
+			Ex. 123-456-[7890]
+		"""
+		if excludeFax:
+			match = re.search(r'^(?!Fax).*(?:\d{1,3}.*)?\d{3}.*\d{3}.*\d{4}', text)
+		else:
+			match = re.search(r'(?:\d{1,3}.*)?\d{3}.*\d{3}.*\d{4}', text)
 		
 		if match:
 			# return the phone number without formatting (only numeric values)
@@ -57,6 +77,17 @@ class BusinessCardParser:
 	def getEmailAddress(self, text):
 		"""Parse a string and return a string containing the email address,
 			or None, if no email address was found."""
+
+		"""Regex: (where brackets indicated matches)
+
+		Description: Match strings where non-whitespace characters
+		surround an @ symbol.
+		Ex: "Email: [john.doe@example.com] test"
+
+		[^\s]*@[^\s]*
+
+		[^\s]* - Match any characters which are not whitespace
+		"""
 		match = re.search(r'[^\s]*@[^\s]*', text)
 
 		if match:
@@ -64,13 +95,14 @@ class BusinessCardParser:
 			return match.group()
 		return None
 
-	def getContactInfo(self, document):
+	def getContactInfo(self, document, showMatches=False):
 		"""Parse a document and return a ContactInfo object.
 			Object values will contain either the respective
 			info or None if no info could be found.
 		
 		Args:
 			document (str): The raw contact info string
+			showMatches (bool): If true, prints all contact info detected
 		
 		Returns:
 			ContactInfo
@@ -94,9 +126,16 @@ class BusinessCardParser:
 			if matchedEmailAddress:
 				emailAddresses.append(matchedEmailAddress)
 		
-		# print("Matched names: ", names)
-		# print("Matched phone numbers: ", phoneNumbers)
-		# print("Matched email addresses", emailAddresses)
+		# For purposes of the challenge, the showMatches parameter
+		# allows the tester to display all info captured by this parser.
+		#
+		# In production code, this functionality should be
+		# seperated from this method.
+		#
+		if showMatches:
+			print("- Matched names: ", names)
+			print("- Matched phone numbers: ", phoneNumbers)
+			print("- Matched email addresses", emailAddresses)
 		
 		return ContactInfo(names[0], phoneNumbers[0], emailAddresses[0])
 		
